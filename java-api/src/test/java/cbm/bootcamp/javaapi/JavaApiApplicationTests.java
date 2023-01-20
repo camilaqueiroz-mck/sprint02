94889de0c7a64a067f9d70fbfdb1ef65527b79c8
Lmam22 <laura_aristizabal_manjarres@mckinsey.com>
2023-01-20 12:13:56 -0500

    Add java-api

---
+++ java-api/src/test/java/cbm/bootcamp/javaapi/JavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cbm.bootcamp.javaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
